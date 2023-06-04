5607f36c754a68156d15761631cb2f4b2d8ffe2f
Devansh <devanshsoni79@gmail.com>
2023-06-04 20:10:04 +0530

    slight changes

--- api/hello/index.go
+++ api/hello/index.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Crade47/vercel-api-testing/utils"
 )
 
-func HandleHello(w http.ResponseWriter, r *http.Request) error {
-	return utils.WriteJSON(w, http.StatusOK, map[string]string{"message": "hello!!"})
+func HandleHello(w http.ResponseWriter, r *http.Request) {
+	if err := utils.WriteJSON(w, http.StatusOK, map[string]string{"message": "hello!!"}); err != nil {
+		fmt.Errorf("%s", err.Error())
+	}
 }
