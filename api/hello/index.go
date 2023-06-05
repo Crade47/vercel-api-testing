443eeabf327d3800fb7eb075014f10104d816fa3
Devansh <devanshsoni79@gmail.com>
2023-06-05 11:15:15 +0530

    private api route testing in go

--- api/hello/index.go
+++ api/hello/index.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/Crade47/vercel-api-testing/middleware"
 	"github.com/Crade47/vercel-api-testing/utils"
 )
 
 func HandleHello(w http.ResponseWriter, r *http.Request) {
+	middleware.EnsureValidToken()(http.HandlerFunc((func(innerW http.ResponseWriter, innerR *http.Request) {
+		helloResponse(w, r)
+	})))
+}
+
+func helloResponse(w http.ResponseWriter, r *http.Request) {
 	if err := utils.WriteJSON(w, http.StatusOK, map[string]string{"message": "hello!!"}); err != nil {
 		fmt.Errorf("%s", err.Error())
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Crade47/vercel-api-testing
 
 go 1.20
+
+require github.com/auth0/go-jwt-middleware/v2 v2.0.0
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
